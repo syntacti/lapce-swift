2dc03fd2ea6d103bf02304b8321d2b390514b14d
Suraj Ghimire <suraj@ghishadow.com>
2023-03-17 09:16:34 +0530

    bump packages and add objc in volt.toml
    Signed-off-by: Suraj Ghimire <suraj@ghishadow.com>

--- README.md
+++ README.md
@@ -6,5 +6,4 @@ Swift plugin for lapce powered by sourcekit-lsp
 
 - go to Settings and add sourcekit-lsp path
 
-
 > The Swift logo is a trademark of Apple Inc.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 use anyhow::{anyhow, Result};
 use lapce_plugin::{
-  psp_types::{
-    lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
-    Request,
-  },
-  register_plugin, LapcePlugin, PLUGIN_RPC,
+    psp_types::{
+        lsp_types::{request::Initialize, DocumentFilter, DocumentSelector, InitializeParams, Url},
+        Request,
+    },
+    register_plugin, LapcePlugin, PLUGIN_RPC,
 };
 use serde_json::Value;
 
@@ -14,94 +14,99 @@ struct State {}
 register_plugin!(State);
 
 macro_rules! string {
-  ( $x:expr ) => {
-    String::from($x)
-  };
+    ( $x:expr ) => {
+        String::from($x)
+    };
 }
 
 macro_rules! ok {
-  ( $x:expr ) => {
-    match ($x) {
-      | Ok(v) => v,
-      | Err(e) => return Err(anyhow!(e)),
-    }
-  };
+    ( $x:expr ) => {
+        match ($x) {
+            Ok(v) => v,
+            Err(e) => return Err(anyhow!(e)),
+        }
+    };
 }
 fn initialize(params: InitializeParams) -> Result<()> {
-  let document_selector: DocumentSelector = vec![
-    DocumentFilter {
-      language: Some(string!("swift")),
-      pattern: Some(string!("**/*.swift")),
-      scheme: None,
-    },
-    DocumentFilter {
-      language: Some(string!("cpp")),
-      pattern: Some(string!("**/*.{H,hh,hpp,h++,C,cc,cpp,c++}")),
-      scheme: None,
-    },
-    DocumentFilter {
-      language: Some(string!("c")),
-      pattern: Some(string!("**/*.{h,c}")),
-      scheme: None,
-    },
-  ];
-  let mut server_args = vec![];
+    let document_selector: DocumentSelector = vec![
+        DocumentFilter {
+            language: Some(string!("swift")),
+            pattern: Some(string!("**/*.swift")),
+            scheme: None,
+        },
+        DocumentFilter {
+            language: Some(string!("cpp")),
+            pattern: Some(string!("**/*.{H,hh,hpp,h++,C,cc,cpp,c++}")),
+            scheme: None,
+        },
+        DocumentFilter {
+            language: Some(string!("c")),
+            pattern: Some(string!("**/*.{h,c}")),
+            scheme: None,
+        },
+        DocumentFilter {
+            language: Some(string!("objective-c")),
+            pattern: Some(string!("**/*.M")),
+            scheme: None,
+        },
+    ];
+    let mut server_args = vec![];
 
-  if let Some(options) = params.initialization_options.as_ref() {
-    if let Some(volt) = options.get("volt") {
-      if let Some(args) = volt.get("serverArgs") {
-        if let Some(args) = args.as_array() {
-          if !args.is_empty() {
-            server_args = vec![];
-          }
-          for arg in args {
-            if let Some(arg) = arg.as_str() {
-              server_args.push(arg.to_string());
+    if let Some(options) = params.initialization_options.as_ref() {
+        if let Some(volt) = options.get("volt") {
+            if let Some(args) = volt.get("serverArgs") {
+                if let Some(args) = args.as_array() {
+                    if !args.is_empty() {
+                        server_args = vec![];
+                    }
+                    for arg in args {
+                        if let Some(arg) = arg.as_str() {
+                            server_args.push(arg.to_string());
+                        }
+                    }
+                }
             }
-          }
-        }
-      }
 
-      if let Some(server_path) = volt.get("serverPath") {
-        if let Some(server_path) = server_path.as_str() {
-          if !server_path.is_empty() {
-            let server_uri = ok!(Url::parse(&format!("urn:{}", server_path)));
-            PLUGIN_RPC.start_lsp(
-              server_uri,
-              server_args,
-              document_selector,
-              params.initialization_options,
-            );
-            return Ok(());
-          }
+            if let Some(server_path) = volt.get("serverPath") {
+                if let Some(server_path) = server_path.as_str() {
+                    if !server_path.is_empty() {
+                        let server_uri = ok!(Url::parse(&format!("urn:{}", server_path)));
+                        PLUGIN_RPC.start_lsp(
+                            server_uri,
+                            server_args,
+                            document_selector,
+                            params.initialization_options,
+                        );
+                        return Ok(());
+                    }
+                }
+            }
         }
-      }
     }
-  }
 
-  let server_uri = ok!(Url::parse("urn:sourcekit-lsp"));
+    let server_uri = ok!(Url::parse("urn:sourcekit-lsp"));
 
-  PLUGIN_RPC.start_lsp(
-    server_uri,
-    server_args,
-    document_selector,
-    params.initialization_options,
-  );
+    PLUGIN_RPC.start_lsp(
+        server_uri,
+        server_args,
+        document_selector,
+        params.initialization_options,
+    );
 
-  Ok(())
+    Ok(())
 }
 
 impl LapcePlugin for State {
-  fn handle_request(&mut self, _id: u64, method: String, params: Value) {
-    #[allow(clippy::single_match)]
-    match method.as_str() {
-      | Initialize::METHOD => {
-        let params: InitializeParams = serde_json::from_value(params).unwrap();
-        if let Err(e) = initialize(params) {
-          PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
+    fn handle_request(&mut self, _id: u64, method: String, params: Value) {
+        #[allow(clippy::single_match)]
+        match method.as_str() {
+            Initialize::METHOD => {
+                let params: InitializeParams = serde_json::from_value(params).unwrap();
+                if let Err(e) = initialize(params) {
+                    PLUGIN_RPC.stderr(&format!("plugin returned with error: {e}"))
+                }
+            }
+            _ => {}
         }
-      }
-      | _ => {}
     }
-  }
 }
